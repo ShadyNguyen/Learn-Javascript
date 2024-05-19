cd93667530045363b5f90a731d6b1a6043688720
ShadyNguyen <sdeadman56@gmail.com>
2024-05-19 07:48:11 +0700

    lesson 1 object trong javascript

---
+++ LearnJS/jsobject/math.js
@@ -0,0 +1,22 @@
+
+
+// math Object
+
+// - Math.PI
+// - Math.round()
+// - Math.abs()
+// - Math.ceil()
+// - Math.floor()
+// - Math.random()
+// - Math.min()
+// - Math.max()
+
+console.log(Math.PI)
+console.log(Math.round(1.1))
+console.log(Math.abs())
+console.log(Math.ceil())
+console.log(Math.floor())
+console.log(Math.random())
+console.log(Math.min())
+console.log(Math.max())
+
