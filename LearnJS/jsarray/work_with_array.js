4a683dbbb4e68b45249164212a47b8a350881729
ShadyNguyen <nguyenbinhduong26@gmail.com>
2024-05-28 22:24:29 +0700

    update work_with_array

--- LearnJS/jsarray/work_with_array.js
+++ LearnJS/jsarray/work_with_array.js
@@ -7,7 +7,7 @@
 // 4.Push
 // 5.Shift
 // 6.Unshift
-// 7.Splicing
+// 7.Splicing (splice)
 // 8.Concat
 // 9.Slicing
 
@@ -17,30 +17,38 @@ var languages = [ // khai báo cách 1
     'Reactjs', 
     'Angular',
 ];
+var languages1 = [ // khai báo cách 1
+    'Nodejs',
+    'Tailwind',
+    'Bootstrap',
+];
 
-console.log(languages.toString()); 
+// console.log(languages.toString()); 
 // chuyển từ array sang kiểu string
 
-console.log(languages.join('-')); 
+// console.log(languages.join('-')); 
 // thêm phần từ trong () vào giữa mảng VD: join('-') => ruby - php - reactjs - angular
 
-console.log(languages.pop()); 
+// console.log(languages.pop()); 
 // xóa elements cuối mảng và trả về phần tử đã xóa
 
-console.log(languages.push('Nodejs')); 
+// console.log(languages.push('Nodejs')); 
 // đẩy thêm phần tử vào cuối mảng và trả về mảng mới có phần từ đã thêm vào
 
-console.log(languages.shift()); 
+// console.log(languages.shift()); 
 // xóa elements đầu mảng và trả về phần tử đã xóa
 
-console.log(languages.unshift('Mysql')); 
+// console.log(languages.unshift('Mysql')); 
 // thêm phần từ mới vào đầu mảng và trả về mảng mới có phần tử đã thêm vào
 
-// console.log(languages.splicing('English')); 
-// chuyển từ array sang kiểu string
+console.log(languages.splice(1,2,'Go', 'Java')); 
+// xóa từ phần tử 1 đến vị trí của phần tử thứ 2 trong mảng 
+// vd trong mảng có ['angular','php','reactjs'] sẽ các vị trí 0,1,2 xóa từ vị trí 1 đến số vị trí 2
+// => còn ['angular'] 
 
-// console.log(languages.concat()); 
-// chuyển từ array sang kiểu string
+// console.log(languages.concat(languages1)); 
+// nối 2 mảng hoặc nhiều hơn hợp nhất thành 1 mảng mới
 
-// console.log(languages.slicing()); 
-// chuyển từ array sang kiểu string
+console.log(languages.slice(0,2)); 
+// trả về phần từ bắt đầu đến phần tử kết thúc 
+// vd trong mảng ['angular,'php','Javascript'] => ['angular','php',]
