9f94624dbf6672170f3e62306fd33cc2d7b09f07
ShadyNguyen <sdeadman56@gmail.com>
2024-06-01 21:43:58 +0700

    Thay đổi tên thư mục

--- LearnJS/js_syntax/ternary_operator.js
+++ LearnJS/js_syntax/ternary_operator.js
@@ -17,3 +17,13 @@ var course = {
 // nếu điều kiện trước "?" đúng thì sẽ in ra điều kiện sau "?" nếu không sẽ in ra điều kiện sau ":" 
 var result = course.coin > 0 ? `${course.coin} Coins` : 'Free';
 console.log(result);
+
+// Làm bài
+function getCanVoteMessage(age){
+    return age >= 18 ? "Bạn có thể bỏ phiếu" : "Bạn chưa được bỏ phiếu"
+}
+
+
+// Kỳ vọng
+console.log(getCanVoteMessage(18)) // 'Bạn có thể bỏ phiếu'
+console.log(getCanVoteMessage(15)) // 'Bạn chưa được bỏ phiếu'
